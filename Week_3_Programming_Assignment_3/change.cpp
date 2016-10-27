8bad30b732de1134807d64e664e581e921da99af
Neil Haria <neilharia007@gmail.com>
2016-10-27 21:41:01 +0530

    Update change.cpp

--- Week_3_Programming_Assignment_3/change.cpp
+++ Week_3_Programming_Assignment_3/change.cpp
@@ -1,3 +1,5 @@
+/* The goal in this problem is to find the minimum number of coins needed to change the input value
+(an integer) into coins with denominations 1, 5, and 10. */
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
